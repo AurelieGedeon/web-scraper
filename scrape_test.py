c60dada774fb55105923d0ad2a3f401292a746fd
Aurelie Gedeon <agedeon540@gmail.com>
2023-08-19 00:01:05 -0400

    added second test varaible and for loop

--- scrape_test.py
+++ scrape_test.py
@@ -4,4 +4,11 @@ import requests
 page_to_scrape = requests.get('https://books.toscrape.com/index.html')
 soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
 
-star_ratings = soup.findAll('p', attrs={'class': 'star-rating'})
+titles = soup.findAll('h3')
+prices = soup.findAll('p', attrs={'class': 'price_color'})
+
+for title in titles:
+    print(title.text)
+
+for price in prices:
+    print(price.text)
