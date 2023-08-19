4759cc44bde77af4bd0c2caac8cc8eb0b263762c
Aurelie Gedeon <agedeon540@gmail.com>
2023-08-18 23:54:19 -0400

    added one web scraping parameter

--- scrape_test.py
+++ scrape_test.py
@@ -3,3 +3,5 @@ import requests
 
 page_to_scrape = requests.get('https://books.toscrape.com/index.html')
 soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
+
+star_ratings = soup.findAll('p', attrs={'class': 'star-rating'})
