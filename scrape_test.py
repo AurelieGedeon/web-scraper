099facaca9c5ed5dd074604200524bbffa10fe9d
Aurelie Gedeon <agedeon540@gmail.com>
2023-08-19 00:09:06 -0400

    imported csv to write titles and their prices

--- scrape_test.py
+++ scrape_test.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 page_to_scrape = requests.get('https://books.toscrape.com/index.html')
 soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
@@ -7,5 +8,11 @@ soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
 titles = soup.findAll('h3')
 prices = soup.findAll('p', attrs={'class': 'price_color'})
 
+file = open('scraped_books.csv', 'w')
+writer = csv.writer(file)
+
+writer.writerow(['Title', 'Price'])
+
 for title, price in zip(titles, prices):
     print(title.text + ' - ' + price.text)
+    writer.writerow([title.text, price.text])
